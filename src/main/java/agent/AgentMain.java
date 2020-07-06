b3683df7d2e8f6603f169355b051f2a20942447e
qqchenkaiyu <37261937+qqchenkaiyu@users.noreply.github.com>
2020-07-06 17:16:59 +0800

    Update AgentMain.java

--- src/main/java/agent/AgentMain.java
+++ src/main/java/agent/AgentMain.java
@@ -3,34 +3,38 @@ package agent;
 import com.sun.tools.attach.VirtualMachine;
 import com.sun.tools.attach.VirtualMachineDescriptor;
 
+import util.WriterLog;
+
 import java.util.List;
 import java.util.StringJoiner;
 
 public class AgentMain {
     public static void main(String[] args) throws Exception {
-        String option=args[0];
-        List<VirtualMachineDescriptor> list =
+        try {
+            String option=args[0];
+            List<VirtualMachineDescriptor> list =
                 VirtualMachine.list();
-        if (option.equals("list")){
-            for (VirtualMachineDescriptor vmd : list) {
-                System.out.println(vmd.displayName());
-            }
-        }else if(option.equals("attach")){
-            String jProcessName = args[1];
-            String agentPath = args[2];
-
-            for (VirtualMachineDescriptor vmd : list) {
-                if(vmd.displayName().endsWith(jProcessName)){
-                    VirtualMachine virtualMachine = VirtualMachine.attach(vmd.id());
-                    StringJoiner stringJoiner = new StringJoiner(" ");
-                    for (int i = 3; i < args.length; i++) {
-                        stringJoiner.add(args[i]);
-                    }
-                   virtualMachine.loadAgent(agentPath,stringJoiner.toString());
-                    System.out.println("loadAgent success");
-                    virtualMachine.detach();
+            if (option.equals("list")){
+                for (VirtualMachineDescriptor vmd : list) {
+                    System.out.println(vmd.displayName());
                 }
+            } else if (option.equals("attach")) {
+                String jid = args[1];
+                String agentPath = args[2];
+                VirtualMachine virtualMachine = VirtualMachine.attach(jid);
+                StringJoiner stringJoiner = new StringJoiner(" ");
+                for (int i = 3; i < args.length; i++) {
+                    stringJoiner.add(args[i]);
+                }
+                virtualMachine.loadAgent(agentPath, stringJoiner.toString());
+                System.out.println("loadAgent success");
+                WriterLog.log("loadAgent success");
+                virtualMachine.detach();
+
             }
+        } catch (Exception e) {
+            WriterLog.error(e);
         }
+
     }
 }
